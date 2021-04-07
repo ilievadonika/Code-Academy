0f49d4cf01128f631abc75f4056659470194ee03
Donika Ilieva <61148450+ilievadonika@users.noreply.github.com>
2021-04-07 19:17:15 +0300

    Update CODE.c

--- Module1_Programming with C/20210322_Individual_Task/CODE.c
+++ Module1_Programming with C/20210322_Individual_Task/CODE.c
@@ -1,14 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
-// #include <string.h>
+#include <string.h>
 #include "header.h"
 
 char strArray[MAX_STRING_LEN];
+int lenStrArray;
+int numLetters = 0;
 
 int main(void) {
     codeLet *legend = generateLegend("CODE.INP.txt");
-
+    
+    for(int i = 0; i < numLetters; i++) {
+        findPosibilities(legend, "", strArray, legend[i]);
+    }
+    
     free(legend);
 
     return 0;
 }
+
+//a 110b 11c 0d 001
