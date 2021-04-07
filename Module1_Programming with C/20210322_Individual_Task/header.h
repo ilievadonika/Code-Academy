5670695517a5848d9c4828936b661ff01b72050e
Donika Ilieva <61148450+ilievadonika@users.noreply.github.com>
2021-04-07 19:18:32 +0300

    Update header.h

--- Module1_Programming with C/20210322_Individual_Task/header.h
+++ Module1_Programming with C/20210322_Individual_Task/header.h
@@ -3,9 +3,22 @@
 #define MAX_POSSIB 10
 
 extern char strArray[MAX_STRING_LEN];
+extern int lenStrArray;
+extern int numLetters;
 
 typedef struct lettersCoding codeLet;
 
-typedef struct possibleString possib; 
-
 codeLet *generateLegend(const char *filename);
+
+void findPosibilities(codeLet *legend, char *decodedLetters, char *leftToDecode, codeLet letterToCheck);
+
+struct lettersCoding {
+    char letter;
+    char code[MAX_CODE_LEN];
+};
+
+struct results {
+    int count;
+    char strings[MAX_POSSIB][MAX_STRING_LEN];
+} possibilities;
+
