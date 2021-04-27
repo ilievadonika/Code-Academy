245dfb11127ec8c4324822d8eb637fbfbc2c0116
Donika Ilieva <61148450+ilievadonika@users.noreply.github.com>
2021-04-27 22:16:11 +0300

    Create Method_Overriding_Example.py

---
+++ Module2_Python_Fundamentals/Method_Overriding_Example.py
@@ -0,0 +1,33 @@
+# Python program to demonstrate 
+# method overriding
+  
+  
+# Defining parent class
+class Parent():
+      
+    # Constructor
+    def __init__(self):
+        self.value = "Inside Parent"
+          
+    # Parent's show method
+    def show(self):
+        print(self.value)
+          
+# Defining child class
+class Child(Parent):
+      
+    # Constructor
+    def __init__(self):
+        self.value = "Inside Child"
+          
+    # Child's show method
+    def show(self):
+        print(self.value)
+          
+          
+# Driver's code
+obj1 = Parent()
+obj2 = Child()
+  
+obj1.show()
+obj2.show()
