f6fb30f9f07a93e711ccd6025ad723255b884380
Donika Ilieva <61148450+ilievadonika@users.noreply.github.com>
2021-05-23 00:15:52 +0300

    Add files via upload

---
+++ Module1_Programming with C/Barcode Scanner - C project/include/Barcode.h
@@ -0,0 +1,8 @@
+#ifndef BARCODE_H_
+#define BARCODE_H_
+
+#include <stdlib.h>
+
+float *readData(size_t *sensorCount);
+
+#endif /* BARCODE_H_ */
---
+++ Module1_Programming with C/Barcode Scanner - C project/include/Defines.h
@@ -0,0 +1,23 @@
+#ifndef DEFINES_H_
+#define DEFINES_H_
+
+/*символ 0      1       2   3       4   5
+кодиране 00001 10001 01001 11001 00101 10100
+символ      6   7       8   9   - / 10 start/stop
+кодиране 01100 00011 10010 10000 00100 00110
+*/
+
+#define ZERO        "00001"
+#define ONE         "10001"
+#define TWO         "01001"
+#define THREE       "11001"
+#define FOUR        "00101"
+#define FIVE        "10100"
+#define SIX         "01100"
+#define SEVEN       "00011"
+#define EIGHT       "10010"
+#define NINE        "10000"
+#define TEN         "00100"
+#define START_STOP  "00110"
+
+#endif /* DEFINES_H_ */
